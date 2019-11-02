45ab24f8e96e55476d75d382007fe5e56f4069e6
Jeff Morhous <jobboy18@gmail.com>
2019-11-02 11:31:08 -0400

    Remove HelloWorld component and move stuff to App.vue for simplicity

--- client/jack-the-resume-ripper/src/App.vue
+++ client/jack-the-resume-ripper/src/App.vue
@@ -16,19 +16,40 @@
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <v-container>
+        <v-layout
+          text-center
+          wrap
+        > 
+          <v-flex
+            xs12
+            mb-5
+          >
+            <v-card
+            class="mx-auto"
+            max-width="344"
+            >
+              <v-card-text>
+                <p class="display-1 text--primary">
+                  API Response
+                </p>
+                <p>Nothing yet</p>
+              </v-card-text>
+            </v-card>
+          </v-flex>
+          <v-flex mb-4>
+            <v-btn outlined color="error">Upload a Resume</v-btn>
+          </v-flex>
+        </v-layout>
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
-  },
   data: () => ({
     //
   }),
