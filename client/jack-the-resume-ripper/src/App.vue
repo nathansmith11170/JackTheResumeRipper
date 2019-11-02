12e200bbc9c260d1c5c1efcec776a976dce92466
Jeff Morhous <jobboy18@gmail.com>
2019-11-02 11:04:32 -0400

    Add a button for upload and a card to display response

--- client/jack-the-resume-ripper/src/App.vue
+++ client/jack-the-resume-ripper/src/App.vue
@@ -2,8 +2,8 @@
   <v-app>
     <v-app-bar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>Jack </span>
+        <span class="font-weight-light">The Resume Ripper</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
@@ -11,7 +11,7 @@
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
       >
-        <span class="mr-2">Latest Release</span>
+        <span class="mr-2">Creators</span>
       </v-btn>
     </v-app-bar>
 
